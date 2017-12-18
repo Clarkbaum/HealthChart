a8257105046708996a5d6acb67dd7f2b63d0fa18
Nathan Clark Baumgartner <nbaumg1193@gmail.com>
2017-12-18 16:36:32 -0600

    header added

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,6 +5,7 @@ import Dialog from 'material-ui/Dialog';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import ReactHighcharts from 'react-highcharts';
+import MERN from '../assets/MERN.jpg';
 
 const convertData = (date) => {
   //Mon Dec 18 2017 15:22:15 GMT-0600 (CST)
@@ -16,44 +17,78 @@ const convertData = (date) => {
 }
 
 const makeChartConfig = (data) => {
-    const graphData = [];
-    console.log("convertData" , convertData(data[0].time))
-    for (let i = 0; i < data.length; i += 1) {
-      graphData.push([Date.UTC(convertData(data[i].time)[0], convertData(data[i].time)[1], convertData(data[i].time)[2]), data[i].heartBeat]);
-    }
-    console.log("graphData", graphData)
-    const config = {
-      title: {
-        text: 'Heart Beat History'
-      },
-      xAxis: {
-        type: 'datetime',
-        dateTimeLabelFormats: { // don't display the dummy year
-          month: '%e. %b',
-          year: '%b'
-        },
-        title: {
-          text: 'Date'
-        }
+  const graphData = [];
+  console.log("convertData" , convertData(data[0].time))
+  for (let i = 0; i < data.length; i += 1) {
+    graphData.push([Date.UTC(convertData(data[i].time)[0], convertData(data[i].time)[1], convertData(data[i].time)[2]), data[i].heartBeat]);
+  }
+  console.log("graphData", graphData)
+  const config = {
+    title: {
+      text: 'HeartBeat History'
     },
-      yAxis: {
-        title: {
-          text: 'Heart Beat'
-        }
+    xAxis: {
+      type: 'datetime',
+      dateTimeLabelFormats: { // don't display the dummy year
+        month: '%e. %b',
+        year: '%b'
       },
-      series: [{
-        name: 'Histogram',
-        type: 'line',
-        data: graphData,
-        pointPadding: 0,
-        groupPadding: 0,
-        pointPlacement: 'between',
-        color: '#3498db'
-      }]
-    };
-
-    return config;
+      title: {
+        text: 'Date'
+      }
+  },
+    yAxis: {
+      title: {
+        text: 'HeartBeat'
+      }
+    },
+    series: [{
+      name: 'HeartBeat',
+      type: 'line',
+      data: graphData,
+      pointPadding: 0,
+      groupPadding: 0,
+      pointPlacement: 'between',
+      color: '#f1a781'
+    }]
   };
+
+  return config;
+};
+
+const styles = {
+  addButton: {
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  header: {
+    backgroundColor: '#2d2d2d',
+    height: 230,
+    paddingTop: 30,
+    textAlign: 'center',
+    marginBottom: 20
+  },
+  topTitle: {
+    fontSize: 34,
+    fontWeight: 600,
+    fontFamily: 'proxima-nova,"Helvetica Neue",Helvetica,Roboto,Arial,sans-serif',
+    color: '#61dafb',
+    margin: 0,
+  },
+  subTitle: {
+    fontFamily: 'proxima-nova,"Helvetica Neue",Helvetica,Roboto,Arial,sans-serif',
+    fontSize: 16,
+    marginBottom: 10,
+    color: 'white'
+  },
+  mernLogo: {
+    height: 50,
+    marginRight: 10
+  },
+  addHeartBeat: {
+    backgroundColor: '#61dafb'
+  }
+}
 
 class App extends React.Component {
   constructor(props) {
@@ -102,10 +137,21 @@ class App extends React.Component {
     window.location.reload();
   }
 
+  
+
   render() {
     return (
       <MuiThemeProvider>
         <div>
+        <div style={styles.header}>
+          <img style={styles.mernLogo} className="logo" src={MERN} alt="MERN" />
+          <div style={styles.topTitle}>Health Charts </div>
+          <div style={styles.subTitle}>your heartbeat history</div> 
+          <FlatButton 
+            label="Add HeartBeat" 
+            style={styles.addHeartBeat}
+          />
+        </div>
           <ReactHighcharts config={makeChartConfig(this.state.data)}/>
         </div>
       </MuiThemeProvider>
